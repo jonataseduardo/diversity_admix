b3012b82e846a6db75a15d955bbb52cd519d46de
Jonatas Cesar <jonataseduardo@gmail.com>
2019-06-20 14:49:25 -0500

    iso curves working for all th for the first time

---
+++ src/admix_plot_utils.py
@@ -0,0 +1,274 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+import coal_theory_utils as ctu
+from numpy import ma
+from matplotlib import cbook
+from matplotlib.colors import Normalize
+from scipy.optimize import fsolve
+from scipy.optimize import brenth
+from scipy.optimize import brentq
+from scipy.optimize import root
+from scipy.optimize import root_scalar
+from scipy.optimize import fixed_point
+from numpy import polyfit, poly1d
+
+
+def set_cmap_levels(max_value, min_value, midpoint=1, digits=1):
+
+    wd = (5 ** ((digits + 1) % 2)) * (1 / 10 ** digits)
+    min_value = min_value - min_value % wd
+    max_value = max_value - max_value % wd + wd
+    lower_levels = np.arange(round(min_value, digits), midpoint, wd)
+    upper_levels = np.arange(midpoint, round(max_value, digits), wd)
+    lower_ticks = lower_levels[::-1][1::2][::-1]
+    upper_ticks = upper_levels[0::2]
+    levels = np.hstack((lower_levels, upper_levels))
+    ticks = np.hstack((lower_ticks, upper_ticks))
+    return (levels, ticks)
+
+
+# Normalizing colormap solition was given in
+# https://stackoverflow.com/a/7404116/
+class MidPointNorm(Normalize):
+    def __init__(self, midpoint=0, vmin=None, vmax=None, clip=False):
+        Normalize.__init__(self, vmin, vmax, clip)
+        self.midpoint = midpoint
+
+    def __call__(self, value, clip=None):
+        if clip is None:
+            clip = self.clip
+
+        result, is_scalar = self.process_value(value)
+
+        self.autoscale_None(result)
+        vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+        if not (vmin < midpoint < vmax):
+            raise ValueError("midpoint must be between maxvalue and minvalue.")
+        elif vmin == vmax:
+            # Or should it be all masked? Or 0.5?
+            result.fill(0)
+        elif vmin > vmax:
+            raise ValueError("maxvalue must be bigger than minvalue")
+        else:
+            vmin = float(vmin)
+            vmax = float(vmax)
+            if clip:
+                mask = ma.getmask(result)
+                result = ma.array(np.clip(result.filled(vmax), vmin, vmax), mask=mask)
+
+            # ma division is very slow; we can take a shortcut
+            resdat = result.data
+
+            # First scale to -1 to 1 range, than to from 0 to 1.
+            resdat -= midpoint
+            resdat[resdat > 0] /= abs(vmax - midpoint)
+            resdat[resdat < 0] /= abs(vmin - midpoint)
+
+            resdat /= 2.0
+            resdat += 0.5
+            result = ma.array(resdat, mask=result.mask, copy=False)
+
+        if is_scalar:
+            result = result[0]
+        return result
+
+    def inverse(self, value):
+        if not self.scaled():
+            raise ValueError("Not invertible until scaled")
+        vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+        if cbook.iterable(value):
+            val = ma.asarray(value)
+            val = 2 * (val - 0.5)
+            val[val > 0] *= abs(vmax - midpoint)
+            val[val < 0] *= abs(vmin - midpoint)
+            val += midpoint
+            return val
+        else:
+            val = 2 * (val - 0.5)
+            if val < 0:
+                return val * abs(vmin - midpoint) + midpoint
+            else:
+                return val * abs(vmax - midpoint) + midpoint
+
+
+def countour_htz(simul_data, alpha_ref, levels):
+
+    Nb_list = simul_data.Nb.unique()
+    t_div_list = simul_data.t_div.unique()
+    Na = simul_data.Na.unique()
+    psize = len(t_div_list), len(Nb_list)
+    data = simul_data[simul_data.alpha == alpha_ref]
+    kappa_list = Nb_list / Na
+    tcoal_list = t_div_list / (2 * Na)
+
+    x = data.t_div.values.reshape(psize) / (2 * Na)
+    y = data.Nb.values.reshape(psize) / Na
+
+    H1 = data.loc[:, "mean_nucleotide_div_pop_a"].values
+    H2 = data.loc[:, "mean_nucleotide_div_pop_c"].values
+    # res = np.log2(H2 / H1)
+    res = H2 / H1
+    z = res.reshape(psize)
+
+    r0 = Nb_list / Na
+
+    # def f(x, args):
+    #    th = args[0]
+    #    fargs = args[1:]
+    #    return func(*fargs, x) - th
+
+    # func = ctu.admix_coal_time_ratio
+    # kappa_ref = kappa_list[-1]
+
+    # def iso_func(th, t_div, alpha):
+    #    t_div, alpha = fargs
+    #    return fsolve(f, x0=0.5, args=(th, t_div, alpha))
+
+    def f(x, args):
+        th, t_div, alpha = args
+        return ctu.admix_coal_time_ratio(t_div, alpha, x) - th
+
+    def iso_func(th, fargs):
+        x_range = kappa_list
+        y_range = f(kappa_list, args=(th, *fargs))
+        pfit = polyfit(x_range, y_range, 10)
+        roots = np.roots(pfit)
+        xmin, xmax = x_range.min(), x_range.max()
+        x0 = roots[
+            np.where(
+                np.logical_and(
+                    np.logical_and(roots.imag == 0, roots.real > 0.5 * xmin),
+                    roots.real < 1.1 * xmax,
+                )
+            )
+        ].real
+        try:
+            return x0.min()
+        except:
+            return np.nan
+
+    # def iso_func(th, fargs):
+    #    x_range = tcoal_list
+    #    y_range = f(tcoal_list, args=(func, th, *fargs))
+    #    pfit = polyfit(x_range, y_range, 10)
+    #    roots = np.roots(pfit)
+    #    xmin, xmax = x_range.min(), x_range.max()
+    #    x0 = roots[
+    #        np.where(
+    #            np.logical_and(
+    #                np.logical_and(roots.imag == 0, roots.real > 0), roots.real < 1.5 * xmax
+    #            )
+    #        )
+    #    ].real
+    #    try:
+    #        return x0.min()
+    #    except:
+    #        return np.nan
+
+    # def iso_th(th):
+    #    iso = []
+    #    for kappa_ref in kappa_list:
+    #        fargs = (alpha_ref, kappa_ref)
+    #        iso += [iso_func(th, fargs)]
+    #    return np.array(iso)
+
+    iso_func(1.0, (0.6, 0.2))
+
+    def iso_th(th):
+        iso = []
+        for t_div in tcoal_list:
+            fargs = (t_div, alpha_ref)
+            # print(fargs)
+            iso += [iso_func(th, fargs)]
+        return np.array(iso)
+
+    iso_h = [iso_th(th) for th in [0.9, 1.0, 1.05, 1.1]]
+    iso_h = [iso_th(th) for th in [0.9, 1.0, 1.1]]
+    len(iso_h[0])
+    iso_th(1)
+
+    cmap_levels, cmap_ticks = set_cmap_levels(z.max(), z.min(), digits=2)
+    cmap = plt.get_cmap("bwr")
+    norm = MidPointNorm(midpoint=1)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title(r"$\alpha={{{}}}$".format(alpha_ref))
+    ax.set_xlabel(r"$\mathrm{time} \, (2 N_a \, \mathrm{coalescent \, units})$")
+    ax.set_ylabel(r"$N_b / N_a$")
+    ax.set_xlim((x.min(), x.max()))
+    im = ax.contourf(x, y, z, levels=cmap_levels, cmap=cmap, norm=norm)
+    for iso in iso_h:
+        ax.plot(tcoal_list, iso)
+    axcb = fig.colorbar(im)
+    axcb.set_label(r"$H_c / H_a$")
+    axcb.set_ticks(cmap_ticks)
+    # fig.savefig("countour_htz_alpha-{}.pdf".format(alpha_ref))
+    fig.show()
+    pass
+
+
+def lines_htz(simul_params, simul_results, Na, alpha_ref):
+
+    t_coal = np.sort(np.unique(simul_params[:, 0])) / (2 * Na)
+    Nb_list = np.sort(np.unique(simul_params[:, 1]))
+
+    idx_alpha = simul_params[:, 2] == alpha_ref
+    ha = simul_results[:, 6]
+    hc = simul_results[:, 7]
+    h = hc / ha
+
+    def get_h(h, Nb_ref):
+        idx_Nb = simul_params[:, 1] == Nb_ref
+        return h[idx_Nb & idx_alpha]
+
+    h_simul_list = [list(zip(t_coal, get_h(h, Nb_ref))) for Nb_ref in Nb_list]
+    h_theory_list = [
+        list(zip(t_coal, ctu.ctu.admix_coal_time_ratio(t_coal, alpha_ref, Nb_ref / Na)))
+        for Nb_ref in Nb_list
+    ]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlim((0, 1))
+    ax.set_ylim((h.min(), h.max()))
+    ax.set_title(r"$\alpha={{{}}}$".format(alpha_ref))
+    ax.set_xlabel(r"$\mathrm{time} \, (2 N_a \, \mathrm{coalescent \, units})$")
+    ax.set_ylabel(r"$H_c / H_a$")
+    line_segments_simul = LineCollection(
+        h_simul_list, linewidths=1.5, linestyles="solid", cmap="viridis_r"
+    )
+    line_segments_simul.set_array(Nb_list / Na)
+    ax.add_collection(line_segments_simul)
+    line_segments_theory = LineCollection(
+        h_theory_list, linewidths=1.3, linestyles="dashed", cmap="viridis_r"
+    )
+    ax.add_collection(line_segments_theory)
+    line_segments_theory.set_array(Nb_list / Na)
+    axcb = fig.colorbar(line_segments_simul)
+    axcb.set_label(r"$N_b / N_a$")
+    fig.savefig("htz_time_alpha-{}.pdf".format(alpha_ref))
+    fig.show()
+    pass
+
+
+def main():
+    simul_data = pd.read_csv("../data/msprime_admix_results_2019-06-18T14:31:39.csv.gz")
+    alpha_list = simul_data.alpha.unique()
+    t_div_list = simul_data.t_div.unique()
+    alpha_ref = alpha_list[1]
+
+    ratio = (
+        simul_data.loc[:, "mean_nucleotide_div_pop_a"].values
+        / simul_data.loc[:, "mean_nucleotide_div_pop_c"].values
+    )
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
